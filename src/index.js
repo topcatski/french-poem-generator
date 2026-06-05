d41e6a2d489fb145d3325025411ba7633c7668b8
Monica Boland <monicaboland2000@gmail.com>
2026-06-05 19:00:24 +0100

    Added basic javascript form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-//alert("Hello world");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
